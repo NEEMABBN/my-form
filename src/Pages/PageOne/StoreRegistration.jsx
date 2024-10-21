cef1bf3face9b2c849462c197aefb93b37de83f1
nima <nima.babanezhad.arimi@gmail.com>
2024-10-21 08:50:15 +0330

    Debug (json)

--- src/Pages/PageOne/StoreRegistration.jsx
+++ src/Pages/PageOne/StoreRegistration.jsx
@@ -31,7 +31,7 @@ export default function StoreRegistration() {
   };
 
   useEffect(() => {
-    fetch("../../JobsData/Gl4B_shop_type.json")
+    fetch("./src/JobsData/Gl4B_shop_type.json")
       .then((response) => response.json())
       .then((data) => setJobsData(data))
       .catch((error) => console.error("Error fetching data : ", error));
